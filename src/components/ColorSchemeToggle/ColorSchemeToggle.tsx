2fef79614d5e399542d76b1f3f3c87a8c0d382f7
S77VEN <stivensg313131@outlook.com>
2024-09-22 05:53:26 -0700

    feat: implement dashboard, navigation navbar, and nested routes

--- src/components/ColorSchemeToggle/ColorSchemeToggle.tsx
+++ src/components/ColorSchemeToggle/ColorSchemeToggle.tsx
@@ -1,13 +1,21 @@
+import { IconDeviceDesktop, IconMoonStars, IconSun } from '@tabler/icons-react';
 import { Button, Group, useMantineColorScheme } from '@mantine/core';
+import classes from './ColorSchemeToggle.module.css';
 
 export function ColorSchemeToggle() {
   const { setColorScheme } = useMantineColorScheme();
 
   return (
-    <Group justify="center" mt="xl">
-      <Button onClick={() => setColorScheme('light')}>Light</Button>
-      <Button onClick={() => setColorScheme('dark')}>Dark</Button>
-      <Button onClick={() => setColorScheme('auto')}>Auto</Button>
+    <Group justify="center" mt="xl" className={classes.footer}>
+      <Button onClick={() => setColorScheme('light')}>
+        <IconSun size={18} />
+      </Button>
+      <Button onClick={() => setColorScheme('dark')}>
+        <IconMoonStars size={18} />
+      </Button>
+      <Button onClick={() => setColorScheme('auto')}>
+        <IconDeviceDesktop size={18} />
+      </Button>
     </Group>
   );
 }
---
+++ src/components/Navbars/NavbarNested/NavbarNested.tsx
@@ -0,0 +1,52 @@
+import {
+  IconAdjustments,
+  IconCalendarStats,
+  IconGauge,
+  IconNotes,
+  IconPresentationAnalytics,
+} from '@tabler/icons-react';
+import { Code, Group, ScrollArea } from '@mantine/core';
+import { ColorSchemeToggle } from '@/components/ColorSchemeToggle/ColorSchemeToggle';
+import { LinksGroup } from './NavbarLinksGroup';
+import classes from './NavbarNested.module.css';
+
+const mockdata = [
+  { label: 'Pagos', icon: IconGauge, link: '/dashboard/pagos' },
+  {
+    label: 'Planilla',
+    icon: IconNotes,
+    initiallyOpened: true,
+    links: [
+      { label: 'Historial', link: '/dashboard/planilla/historial' },
+      { label: 'Calcular', link: '/dashboard/planilla/calcular' },
+    ],
+  },
+  {
+    label: 'Departamentos',
+    icon: IconCalendarStats,
+    links: [
+      { label: 'Administrar', link: '/dashboard/departamentos/administrar' },
+      { label: 'Consultar', link: '/dashboard/departamentos/consultar' },
+    ],
+  },
+  { label: 'Colaboradores', icon: IconPresentationAnalytics, link: '/dashboard/colaboradores' },
+  { label: 'Configuración', icon: IconAdjustments, link: '/dashboard/configuracion' },
+];
+
+export function NavbarNested() {
+  const links = mockdata.map((item) => <LinksGroup {...item} key={item.label} />);
+
+  return (
+    <nav className={classes.navbar}>
+      <div className={classes.header}>
+        <Group justify="space-between">
+          <Code fw={700}>Evasion Fiscal TEC</Code>
+        </Group>
+      </div>
+      <ScrollArea className={classes.links}>
+        <div className={classes.linksInner}>{links}</div>
+      </ScrollArea>
+      <ColorSchemeToggle />
+    </nav>
+  );
+}
