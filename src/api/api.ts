eb13f06b4186748f98de9220b9f088f20674ecf7
S77VEN <stivensg313131@outlook.com>
2024-10-06 08:30:19 -0700

    feat: implement config for .env in axios

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,10 @@
+;
 // Axios
 import axios from 'axios';
 
+
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     'Access-Control-Allow-Origin': '*',
     'Content-Type': 'application/json',
