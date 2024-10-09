c4b2cae7a6b96b51479a395da9ce6733695d2ac9
Daniel Granados <dandiegogranados@yahoo.com>
2024-10-08 19:45:11 -0600

    fix: return empty list in getquincenastotal

--- db_scripts/StoredProcedures/getquincenastotal.sql
+++ db_scripts/StoredProcedures/getquincenastotal.sql
@@ -26,7 +26,7 @@ DECLARE
 	l_fechafin DATE;
 BEGIN
 	queryStr := '
-    SELECT SUM(s.salariobruto),
+    SELECT SUM(s.salariobruto) salariobruto,
            SUM(p.pateym), SUM(p.pativm), SUM(p.obreym), SUM(p.obrivm), SUM(p.obrbanco), 
            SUM(p.obrsolidarista), SUM(p.resaguinaldo), SUM(p.rescesantia), SUM(p.resvacaciones), 
            SUM(p.impuestorenta)
@@ -75,6 +75,7 @@ BEGIN
 		END IF;
 	END IF;
 
+	queryStr := queryStr || ' HAVING SUM(s.salariobruto) IS NOT NULL';
     -- Execute the dynamic query
     RETURN QUERY EXECUTE queryStr USING p_fechapago, l_fechafin, p_cedula, p_departamentoId;
 END;
@@ -82,7 +83,7 @@ $$;
 
 DROP FUNCTION getquincenastotal(date, integer, smallint)
 
-SELECT * FROM getquincenastotal('2024-10-05'::DATE, NULL::DATE, NULL::INT, NULL::SMALLINT)
+SELECT * FROM getquincenastotal('2024-11-14'::DATE, NULL::DATE, NULL::INT, NULL::SMALLINT)
 SELECT * FROM getquincenastotal(NULL::DATE, NULL::DATE, NULL::INT, NULL::SMALLINT)
 SELECT * FROM getquincenastotal(NULL::DATE, '2024-10-05'::DATE, NULL::INT, NULL::SMALLINT)
 SELECT * FROM getquincenastotal(NULL::DATE, NULL::DATE, 105750751::INT, NULL::SMALLINT)
@@ -90,3 +91,18 @@ SELECT * FROM getquincenastotal(NULL::DATE, NULL::DATE, NULL::INT, 10::SMALLINT)
 SELECT * FROM getquincenas(NULL::DATE, NULL::DATE, 100022263::INT, 3::SMALLINT, NULL::INT, NULL::INT);
 
 SELECT * FROM salarios ORDER BY salariobruto DESC LIMIT 3
+
+SELECT SUM(s.salariobruto) salariobruto,
+           SUM(p.pateym), SUM(p.pativm), SUM(p.obreym), SUM(p.obrivm), SUM(p.obrbanco), 
+           SUM(p.obrsolidarista), SUM(p.resaguinaldo), SUM(p.rescesantia), SUM(p.resvacaciones), 
+           SUM(p.impuestorenta)
+    FROM public.pagos p
+    INNER JOIN empleadosdepartamentos ed ON ed.cedula = p.cedula
+    INNER JOIN empleados e ON e.cedula = p.cedula
+    INNER JOIN salarios s ON s.salarioid = p.salarioid
+    INNER JOIN nombres n ON n.nombreId = e.nombreId
+    INNER JOIN apellidos a1 ON a1.apellidoId = e.apellido1Id
+    INNER JOIN apellidos a2 ON a2.apellidoId = e.apellido2Id
+    INNER JOIN departamentos d ON d.departamentoId = ed.departamentoId
+	WHERE p.fechapago::DATE = '2024-11-05'
+	HAVING SUM(s.salariobruto) IS NOT NULL
